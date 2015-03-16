5b913bb889d6cc16df995a28c49e6f6ddbc18c1b
Cheng, Xi <byroneagle@hotmail.com>
2015-03-16 13:11:11 +0100

    use rawinput to set sender mail and password in manage.py runserver

--- config.py
+++ config.py
@@ -5,14 +5,22 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
+    #MAIL_SERVER = 'smtp.live.com'
+    #MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    #MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_USERNAME = raw_input('MAIL_USERNAME: ')
+    #MAIL_USERNAME = "byroneagle@hotmail.com"
+    #MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_PASSWORD = raw_input('MAIL_PASSWORD: ')
+    #MAIL_PASSWORD = "flyeagle1982A"
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    #FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_ADMIN = raw_input('FLASKY_ADMIN: ')
+    #FLASKY_ADMIN = "byroneagle@gmail.com"
 
     @staticmethod
     def init_app(app):
